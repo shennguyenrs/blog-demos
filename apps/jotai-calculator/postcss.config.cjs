3e499fac491be7b82b68aeaea68f78215b7c81e7
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2022-11-21 13:57:54 +0200

    feat: add jotai-calculator

---
+++ apps/jotai-calculator/postcss.config.cjs
@@ -0,0 +1,13 @@
+module.exports = require("tailwind-config/postcss.config.cjs");
+
+/*
+Or you can config like this without create tailwind.config.css
+const config = require('tailwind-config/tailwind.config.cjs')
+
+module.exports = {
+    plugins:{
+        tailwindcss: { config },
+        autoprefixer: {}
+    }
+}
+*/
