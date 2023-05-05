8e77f0b2c299313341d0eb6b3952bc5ba9aaa1e0
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2023-05-06 00:28:56 +0300

    feat(jotai-signal): add unocss

--- apps/jotai-signals/src/App.tsx
+++ apps/jotai-signals/src/App.tsx
@@ -2,13 +2,17 @@
 
 import { atom, useAtom } from "jotai";
 import { $ } from "jotai-signal";
+import "virtual:uno.css";
 
 const counterAtom = atom(0);
 
 function Controller() {
   const [, setCounter] = useAtom(counterAtom);
   return (
-    <button onClick={() => setCounter((c: number) => c + 1)}>
+    <button
+      className="bg-teal-200 hover:bg-teal-300 text-teal-700 font-bold py-2 px-4 rounded border-0 transition ease-in-out active:scale-95"
+      onClick={() => setCounter((c: number) => c + 1)}
+    >
       Add one to counter
     </button>
   );
@@ -18,7 +22,7 @@ function Display() {
   const [value] = useAtom(counterAtom);
   return (
     <p>
-      'useAtom': {value} - {Math.random()}
+      Using 'useAtom': {value} - {Math.random()}
     </p>
   );
 }
@@ -26,22 +30,22 @@ function Display() {
 function DisplayWithSignal() {
   return (
     <p>
-      Signal: {$(counterAtom)} - {Math.random()}
+      Using Signal: {$(counterAtom)} - {Math.random()}
     </p>
   );
 }
 
 function App() {
   return (
-    <>
-      <Controller />
-      <p>
-        New number will be shown after the counter when the component is
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <h3>
+        New number will be shown (next to the counter) when the component is
         re-render
-      </p>
+      </h3>
+      <Controller />
       <Display />
       <DisplayWithSignal />
-    </>
+    </div>
   );
 }
 
